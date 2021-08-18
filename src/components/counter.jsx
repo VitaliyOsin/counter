efab2013b700e30a7d206444f6a81f7ef51c141b
VitaliyOsin <d_dva@list.ru>
2021-08-19 00:18:26 +0700

    19-08-2021 00:18

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,21 +2,42 @@ import React, { Fragment, useState } from "react";
 
 const Counter = () => {
   const [counter, setCounter] = useState(0);
-  const tags = ["tag1", "tag2", "tag3"];
   const formCount = counter === 0 ? "Ноль" : counter;
   const getBageClasses = () => {
     let classes = "badge m-2 bg-";
     classes += counter === 0 ? "danger" : "primary";
     return classes;
   };
+
+  const handleIncrement = () => {
+    setCounter(counter + 1);
+  };
+
+  const handleDecrement = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
+
   return (
     <Fragment>
-      {tags.length === 0 && "Теги не найдены"}
-      {tags.map((tag) => (
-        <li key={tag}>{tag}</li>
-      ))}
       <span className={getBageClasses()}>{formCount}</span>
-      <button className="btn btn-secondary btn-sm">Increament</button>
+      <button
+        onClick={() => {
+          handleIncrement({ id: 1 });
+        }}
+        className="btn btn-secondary btn-sm"
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          handleDecrement({ id: 2 });
+        }}
+        className="btn btn-secondary btn-sm"
+      >
+        Decrement
+      </button>
     </Fragment>
   );
 };
